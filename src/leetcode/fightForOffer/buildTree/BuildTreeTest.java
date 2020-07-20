8641adbe1e132648f48b88a7a67809a61795035f
liangze <ze.liang@huoquanquan.cn>
2020-07-20 14:55:32 +0800

    重建二叉树

--- src/leetcode/fightForOffer/buildTree/BuildTreeTest.java
+++ src/leetcode/fightForOffer/buildTree/BuildTreeTest.java
@@ -1,17 +1,36 @@
 package leetcode.fightForOffer.buildTree;
 
+import java.util.HashMap;
+
 /**
  * @author zzzz
  * @create 2020-06-22 下午12:28
  */
 public class BuildTreeTest {
-    TreeNode result=null;
+    HashMap<Integer, Integer> dic = new HashMap<>();
+    int[] po;
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        TreeNode treeNode=new TreeNode(preorder[0]);
-        return treeNode;
+        po = preorder;
+        for(int i = 0; i < inorder.length; i++)
+            dic.put(inorder[i], i);
+        return recur(0, 0, inorder.length - 1);
+    }
+    TreeNode recur(int pre_root, int in_left, int in_right) {
+        if(in_left > in_right) return null;
+        TreeNode root = new TreeNode(po[pre_root]);
+        int i = dic.get(po[pre_root]);
+        root.left = recur(pre_root + 1, in_left, i - 1);
+        root.right = recur(pre_root + i - in_left + 1, i + 1, in_right);
+        return root;
+    }
 
+    public static void main(String[] args) {
+        int[] preorder = {3,9,20,15,7};
+        int[] inorder = {9,3,15,20,7};
+        BuildTreeTest test = new BuildTreeTest();
+        TreeNode result =test.buildTree(preorder,inorder);
+        System.out.println(result.toString());
     }
-    public TreeNode findRoot(){
-        return null;
-    }
+
+
 }
