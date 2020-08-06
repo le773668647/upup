de24ecf3e320ea09e0e0675c15d679d9824409e3
liangze <ze.liang@huoquanquan.cn>
2020-08-06 17:25:35 +0800

    计算时间

--- src/main/java/exercise/ExerciseTest.java
+++ src/main/java/exercise/ExerciseTest.java
@@ -1,11 +1,10 @@
 package exercise;
 
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -14,19 +13,27 @@ import java.util.stream.Collectors;
  * @create 2020-07-25 下午12:06
  */
 public class ExerciseTest {
+    private static final Logger logger = LoggerFactory.getLogger(ExerciseTest.class);
+
     public static void main(String[] args) {
 //        List<Person> personList = new ArrayList<>();
 //        personList.add(new Person("a",12));
 //        personList.add(new Person("b",13));
 //        Map<String,Person> map =personList.stream().collect(Collectors.toMap(Person::getName, Function.identity()));
 //        System.out.println(map);
-//        System.out.println(Arrays.asList(",".split(","))
-//                .parallelStream()
-//                .map(a -> Long.parseLong(a.toString()))
-//                .collect(Collectors.toList()).size());
-        String a = "";
+        System.out.println(Arrays.asList("a".split("@"))
+                .parallelStream()
+                .collect(Collectors.toList()));
+        String a = "   a  b ";
+        System.out.println(a.trim());
         System.out.println(StringUtils.isBlank(a));
         int c = 1;
+        logger.info(""+c);
+        String s = "a";
+        String [] array = s.split("@");
+        System.out.println(array[1]);
+        String [] f = {"A","B","C"};
+        System.out.println(new Random().nextInt(f.length));
 
     }
 }
