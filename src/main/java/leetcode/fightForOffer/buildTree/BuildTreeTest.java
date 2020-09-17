03d83912f418a8441854010406962cf6ac2f1b89
liangze <ze.liang@huoquanquan.cn>
2020-09-17 18:19:37 +0800

    重建二叉树自我梳理

--- src/main/java/leetcode/fightForOffer/buildTree/BuildTreeTest.java
+++ src/main/java/leetcode/fightForOffer/buildTree/BuildTreeTest.java
@@ -5,39 +5,50 @@ import java.util.Map;
 
 /**
  * @author zzzz
+ * 重建二叉树 前序遍历和中序遍历
  * @create 2020-06-22 下午12:28
  */
 public class BuildTreeTest {
-
-    //存放的是中序遍历,值和index
-    HashMap<Integer, Integer> dic = new HashMap<>();
+    //存放前序遍历数组
     int[] po;
+    //存放中序遍历的值和index
+    Map<Integer, Integer> indexMap = new HashMap<>();
+
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         po = preorder;
-        for(int i = 0; i < inorder.length; i++){
-            dic.put(inorder[i], i);
+        for (int i = 0; i < inorder.length; i++) {
+            indexMap.put(inorder[i], i);
         }
-        return recur(0, 0, inorder.length - 1);
+        return buildTree(0, 0, inorder.length - 1);
+
     }
 
-    TreeNode recur(int pre_root, int in_left, int in_right) {
-        if(in_left > in_right) return null;
-        TreeNode root = new TreeNode(po[pre_root]);
-        int i = dic.get(po[pre_root]);
-        root.left = recur(pre_root + 1, in_left, i - 1);
-        root.right = recur(pre_root + i - in_left + 1, i + 1, in_right);
+    /**
+     * @param pre_root_idx 前序遍历下根节点的index
+     * @param in_left_idx  中序遍历下最左节点index
+     * @param in_right_idx 中序遍历下最右节点的index
+     * @return
+     */
+    private TreeNode buildTree(int pre_root_idx, int in_left_idx, int in_right_idx) {
+        //子树的左边界大于右边界说明已经没有了
+        if (in_left_idx > in_right_idx) {
+            return null;
+        }
+        //创建根节点,为根赋值
+        TreeNode root = new TreeNode(po[pre_root_idx]);
+        //拿到中序遍历根的index
+        int rootIndexOfInorder = indexMap.get(po[pre_root_idx]);
+        root.left = buildTree(pre_root_idx + 1, in_left_idx, rootIndexOfInorder - 1);
+        //右子树的根 = 当前根 + 左子树的长度
+        //中序遍历右子数的左边界 = 中序遍历下根 + 1
+        root.right = buildTree(pre_root_idx + (rootIndexOfInorder - in_left_idx) + 1, rootIndexOfInorder + 1, in_right_idx);
         return root;
     }
 
     public static void main(String[] args) {
-        int[] preorder = {3,9,20,15,7};
-        int[] inorder = {9,3,15,20,7};
+        int [] a = {3,9,20,15,7};
+        int [] b = {9,3,15,20,7};
         BuildTreeTest test = new BuildTreeTest();
-        TreeNode result =test.buildTree(preorder,inorder);
-        System.out.println(result.toString());
+        test.buildTree(a,b);
     }
-
-
-
-
 }
