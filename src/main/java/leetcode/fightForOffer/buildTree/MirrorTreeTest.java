eb3c267e70835c1bf21cd682756dcbda78becb25
liangze <ze.liang@huoquanquan.cn>
2020-09-18 14:12:47 +0800

    二叉树的镜像

---
+++ src/main/java/leetcode/fightForOffer/buildTree/MirrorTreeTest.java
@@ -0,0 +1,25 @@
+package leetcode.fightForOffer.buildTree;
+
+/**
+ * @author zzzz
+ * @create 2020-06-18 下午7:53
+ * 剑指offer26 树的子结构
+ */
+public class MirrorTreeTest {
+    public TreeNode mirrorTree(TreeNode root) {
+        //根为空直接返回
+        if(root == null ){
+            return root;
+        }
+        //声明临时变量,不然会覆盖的
+        TreeNode left=root.left;
+        TreeNode right=root.right;
+        //递归交换
+        root.left = mirrorTree(right);
+        root.right = mirrorTree(left);
+        return root;
+
+    }
+
+
+}
